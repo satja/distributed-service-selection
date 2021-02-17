e6c9ba4d79191f5589ba7c65692f8aceb9ba94a6
Adrian Satja Kurdija <askurdija@gmail.com>
2021-02-17 14:42:46 +0100

    rearrange plot

--- plot.py
+++ plot.py
@@ -1,10 +1,18 @@
 from collections import defaultdict, OrderedDict
 import matplotlib.pyplot as plt
+import numpy as np
+
+def get_label(name):
+    if name != 'single broker':
+        name = 'multi-broker + ' + name
+        name.replace('&', ' & ')
+    return name
 
 plt.rcParams.update({'font.size': 18})
-plt.figure(figsize=(20,10))
+#plt.figure(figsize=(20,10))
 for name in ('Cost', 'Successful reqs.', 'Failed reqs.',\
         'Violated RT reqs.', 'Violated reliability reqs.'):
+    plt.figure(figsize=(20,10))
     res = defaultdict(float)
     cnt = defaultdict(int)
     with open(name + '.txt') as f:
@@ -19,25 +27,23 @@ for name in ('Cost', 'Successful reqs.', 'Failed reqs.',\
     #colors = []
     colors = ['black', 'purple', 'green', 'blue', 'red']
     labels = []
-    for i, broker in enumerate(['single broker', 'no balancing',\
-            'user balancing', 'service balancing', 'user&service balancing']):
-        #colors.extend(['black', 'purple', 'green', 'blue', 'red'])
-        for j, alg in enumerate(['Random', 'Round Robin', 'Greedy', 'AP', "TP"]):
+    plt.grid()
+    for i, alg in enumerate(['Random', 'Round Robin', 'Greedy', 'AP', "TP"]):
+        for j, broker in enumerate(['single broker', 'no balancing',\
+                'user balancing', 'service balancing', 'user&service balancing']):
             positions.append(i * 6 + j)
             heights.append(res[(alg, broker)] / cnt[(alg, broker)])
-            #labels.append(alg)
             plt.bar(i * 6 + j, res[(alg, broker)] / cnt[(alg, broker)], align='center',
-                    color= colors[j], label=alg + ' Selection')
-        names.append(broker)
+                    color= colors[j], label=get_label(broker))
+        names.append(alg + ' Selection')
         name_positions.append(i * 6 + 2)
+    plt.yticks(np.arange(0,max(heights)+0.05,0.1))
     #plt.bar(positions, heights, align='center', color=colors, label=labels)
     plt.xticks(name_positions, names)  #, rotation=45)
-    plt.title(name.replace('Cost', "Average cost"))
+    plt.title(name.replace('Cost', "Average cost").replace('Succ', 'QoS-succ'))
     handles, labels = plt.gca().get_legend_handles_labels()
     by_label = OrderedDict(zip(labels, handles))
     plt.legend(by_label.values(), by_label.keys())
-    #plt.legend()
     #plt.show()
     plt.savefig(name + '.pdf', format='pdf')
     plt.clf()
-    #exit(0)
